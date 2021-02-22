ae781eb1a109a86294fdf15ce13f43cefcab4038
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2021-02-22 10:44:18 -0300

    [NLW#4] Rumo ao Próximo Nível [Node.js]

---
+++ api/src/server.ts
@@ -0,0 +1,15 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Olá" });
+})
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Os dados foram salvos com sucesso!" });
+})
+
+
+
+app.listen(3333, () => console.log("Server is running!"));
